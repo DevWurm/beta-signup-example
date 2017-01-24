f859ba112ba4e184bec22cb27e38ef9e0e9b524a
DevWurm <devwurm@devwurm.net>
2017-01-24 15:38:46 +0100

    Updating App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BetaComponent } from './beta/beta.component';
+import { BetaSignupComponent } from './beta/beta-signup/beta-signup.component';
+import { BetaSuccessComponent } from './beta/beta-success/beta-success.component';
+import { routing } from './app.routing';
+import { LoginGuardService } from './shared/login-guard.service';
+import { BetaGuardService } from './shared/beta-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BetaComponent,
+    BetaSignupComponent,
+    BetaSuccessComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [LoginGuardService, BetaGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
